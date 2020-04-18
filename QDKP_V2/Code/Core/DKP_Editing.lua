2be8b4e2e45bfdf2b1b2d79fae39b60c67998eca
Homerocker <molodoy3561@gmail.com>
2020-04-18 15:30:50 +0300

    look for online alts to send whispers to if target character is offline

--- QDKP_V2/Code/Core/DKP_Editing.lua
+++ QDKP_V2/Code/Core/DKP_Editing.lua
@@ -1,4 +1,3 @@
-
 -- Copyright 2010 Riccardo Belloli (belloli@email.it)
 -- This file is a part of QDKP_V2 (see about.txt in the Addon's root folder)
 
@@ -34,147 +33,158 @@
 -- doNotReset: by default, this function will reset the player data on errors. Set this to true to hinibit.
 
 function QDKP2_GetTotal(name, doNotReset)
-  name=QDKP2_GetMain(name)
+  name = QDKP2_GetMain(name)
   if not QDKP2rankIndex[name] then
-    QDKP2_Debug(1,"Core","Asked total DKP of "..tostring(name).." but he's not in guild.")
+    QDKP2_Debug(1, "Core", "Asked total DKP of " .. tostring(name) .. " but he's not in guild.")
     return 0
   end
   if not QDKP2note[name] then
-    QDKP2_Debug(1,"Core","Asked total DKP of "..tostring(name).." but he has not DKP values?!?!?")
+    QDKP2_Debug(1, "Core", "Asked total DKP of " .. tostring(name) .. " but he has not DKP values?!?!?")
     GuildRoster()
     return 0
   end
-  local Total=QDKP2note[name][QDKP2_TOTAL]
+  local Total = QDKP2note[name][QDKP2_TOTAL]
   if not Total then
-    QDKP2_Debug(1, "Core","Error while getting Total amount for "..name..". Doesn't have a valid data array.")
-	if doNotReset then Total=0
-	else
-	  QDKP2_ResetPlayer(name)
-	  Total=QDKP2_GetTotal(name,true)
-	end
+    QDKP2_Debug(1, "Core", "Error while getting Total amount for " .. name .. ". Doesn't have a valid data array.")
+    if doNotReset then
+      Total = 0
+    else
+      QDKP2_ResetPlayer(name)
+      Total = QDKP2_GetTotal(name, true)
+    end
   end
   return Total
 end
 
 function QDKP2_GetSpent(name, doNotReset)
-  name=QDKP2_GetMain(name)
+  name = QDKP2_GetMain(name)
   if not QDKP2rankIndex[name] then
-    QDKP2_Debug(1,"Core","Asked total DKP of "..tostring(name).." but he's not in guild.")
+    QDKP2_Debug(1, "Core", "Asked total DKP of " .. tostring(name) .. " but he's not in guild.")
     return 0
   end
   if not QDKP2note[name] then
-    QDKP2_Debug(1,"Core","Asked total DKP of "..tostring(name).." but he has not DKP values?!?!?")
+    QDKP2_Debug(1, "Core", "Asked total DKP of " .. tostring(name) .. " but he has not DKP values?!?!?")
     GuildRoster()
     return 0
   end
-  local Spent=QDKP2note[name][QDKP2_SPENT]
+  local Spent = QDKP2note[name][QDKP2_SPENT]
   if not Spent then
-    QDKP2_Debug(1, "Core","Error while getting Spent amount for "..name..". Doesn't have a valid data array,")
-	if doNotReset then Spent=0
-	else
-	  QDKP2_ResetPlayer(name)
-	  Spent=QDKP2_GetSpent(name,true)
-	end
+    QDKP2_Debug(1, "Core", "Error while getting Spent amount for " .. name .. ". Doesn't have a valid data array,")
+    if doNotReset then
+      Spent = 0
+    else
+      QDKP2_ResetPlayer(name)
+      Spent = QDKP2_GetSpent(name, true)
+    end
   end
   return Spent
 end
 
 function QDKP2_GetNet(name)
-  return QDKP2_GetTotal(name)-QDKP2_GetSpent(name)
+  return QDKP2_GetTotal(name) - QDKP2_GetSpent(name)
 end
 
 function QDKP2_GetHours(name, doNotReset)
-  name=QDKP2_GetMain(name)
+  name = QDKP2_GetMain(name)
   if not QDKP2rankIndex[name] then
-    QDKP2_Debug(1,"Core","Asked total DKP of "..tostring(name).." but he's not in guild.")
+    QDKP2_Debug(1, "Core", "Asked total DKP of " .. tostring(name) .. " but he's not in guild.")
     return 0
   end
   if not QDKP2note[name] then
-    QDKP2_Debug(1,"Core","Asked total DKP of "..tostring(name).." but he has not DKP values?!?!?")
+    QDKP2_Debug(1, "Core", "Asked total DKP of " .. tostring(name) .. " but he has not DKP values?!?!?")
     GuildRoster()
     return 0
   end
   if not QDKP2note[name] then
-    QDKP2_Debug(1,"Core","Asked hours of "..tostring(name).." but he's not in guild.")
+    QDKP2_Debug(1, "Core", "Asked hours of " .. tostring(name) .. " but he's not in guild.")
     GuildRoster()
     return 0
   end
-  local Hours=QDKP2note[name][QDKP2_HOURS]
+  local Hours = QDKP2note[name][QDKP2_HOURS]
   if not Hours then
-        QDKP2_Debug(1, "Core","Error while getting Hours amount for "..name..". Doesn't have a valid data array,")
-	if doNotReset then Hours=0
-	else
-	  QDKP2_ResetPlayer(name)
-	  Hours=QDKP2_GetHours(name,true)
-	end
+    QDKP2_Debug(1, "Core", "Error while getting Hours amount for " .. name .. ". Doesn't have a valid data array,")
+    if doNotReset then
+      Hours = 0
+    else
+      QDKP2_ResetPlayer(name)
+      Hours = QDKP2_GetHours(name, true)
+    end
   end
   return Hours
 end
 
-
-function QDKP2_GetTotSpentRatio(name,doNOtReset)
---returns the quotient between Total and Spent DKP. This is to add a EP/GP support to QDKP2.
---returns a float. If Spent is 0 returns 9999
-  local Total=QDKP2_GetTotal(name,doNotReset)
-  local Spent=QDKP2_GetTotal(name,doNotReset)
-  if Spent==0 then return 9999; end
+function QDKP2_GetTotSpentRatio(name, doNOtReset)
+  --returns the quotient between Total and Spent DKP. This is to add a EP/GP support to QDKP2.
+  --returns a float. If Spent is 0 returns 9999
+  local Total = QDKP2_GetTotal(name, doNotReset)
+  local Spent = QDKP2_GetTotal(name, doNotReset)
+  if Spent == 0 then
+    return 9999;
+  end
   return (Total / Spent)
 end
 
 function QDKP2_GetSessionAmounts(name)
   local G, S
-  local sessions=QDKP2_IsSessionPresent()
-  if #sessions>0 then
-    G=0; S=0
-    for i,SID in pairs(sessions) do
-      local LogList=QDKP2log_GetPlayer(SID,name)
-      if LogList and #LogList>0 then
-        local sums=QDKP2log_ResumDKPChanges(LogList)
-        G=G+(sums[1] or 0)
-        S=S+(sums[2] or 0)
+  local sessions = QDKP2_IsSessionPresent()
+  if #sessions > 0 then
+    G = 0;
+    S = 0
+    for i, SID in pairs(sessions) do
+      local LogList = QDKP2log_GetPlayer(SID, name)
+      if LogList and #LogList > 0 then
+        local sums = QDKP2log_ResumDKPChanges(LogList)
+        G = G + (sums[1] or 0)
+        S = S + (sums[2] or 0)
       end
     end
   end
-  return G,S
+  return G, S
 end
 
-
 function QDKP2_AddTotals(name, award, spent, hours, reason, noMsgIt, timestamp, noLogIt, outsideSession)
--- this adds to the value in the local varables that store the DKPs (both total and cumulative)
--- Usage = QDKP2_AddTotals(name, award, spent, hours, reason, NoMsg, timestamp, NoLog)
--- award, spent and hours are the amounts to add/subtract.
--- reason is an optional string that describes the operation. Used for log and for the message.
--- noMsgIt suppress the printing of the operation.
--- *********** The next arguments should NOT be used when the function is called as API. *************
--- Timestamp is used to create linked log entries.
--- noLogit suppress the creation of a log entry with the operation done.
---
--- all arguments are optionals except for the name.
--- you can pass a string like "10%" for total and/or spent, and it will be calculated in relation with net DKP.
--- logIt will add a DKP log entry.
--- msgIt will display a text of the modification to the given channel.
+  -- this adds to the value in the local varables that store the DKPs (both total and cumulative)
+  -- Usage = QDKP2_AddTotals(name, award, spent, hours, reason, NoMsg, timestamp, NoLog)
+  -- award, spent and hours are the amounts to add/subtract.
+  -- reason is an optional string that describes the operation. Used for log and for the message.
+  -- noMsgIt suppress the printing of the operation.
+  -- *********** The next arguments should NOT be used when the function is called as API. *************
+  -- Timestamp is used to create linked log entries.
+  -- noLogit suppress the creation of a log entry with the operation done.
+  --
+  -- all arguments are optionals except for the name.
+  -- you can pass a string like "10%" for total and/or spent, and it will be calculated in relation with net DKP.
+  -- logIt will add a DKP log entry.
+  -- msgIt will display a text of the modification to the given channel.
 
-  QDKP2_Debug(2,"Core","Calling AddTotal for "..name..". +DKP="..tostring(award)..", -DKP="..tostring(spent)..", Hours="..tostring(hours)..", Coeff="..tostring(coefficent))
+  QDKP2_Debug(2, "Core", "Calling AddTotal for " .. name .. ". +DKP=" .. tostring(award) .. ", -DKP=" .. tostring(spent) .. ", Hours=" .. tostring(hours) .. ", Coeff=" .. tostring(coefficent))
 
-  if not QDKP2_OfficerMode() then QDKP2_Msg(QDKP2_LOC_NoRights,"ERROR")(); return; end
+  if not QDKP2_OfficerMode() then
+    QDKP2_Msg(QDKP2_LOC_NoRights, "ERROR")();
+    return ;
+  end
 
   if not QDKP2_IsInGuild(name) then
-    QDKP2_Debug(1,"Core","Calling AddTotals with a player that is not in the guild!")
+    QDKP2_Debug(1, "Core", "Calling AddTotals with a player that is not in the guild!")
     return
   end
 
-  if type(award)=="string" then award=QDKP2_GetAmountFromText(award,name); end
-  if type(spent)=="string" then spent=QDKP2_GetAmountFromText(spent,name); end
+  if type(award) == "string" then
+    award = QDKP2_GetAmountFromText(award, name);
+  end
+  if type(spent) == "string" then
+    spent = QDKP2_GetAmountFromText(spent, name);
+  end
+  z
+  hours = tonumber(hours)
 
-  hours=tonumber(hours)
+  coeff = tonumber(coeff)
 
-  coeff=tonumber(coeff)
-
-  local OriginalName=name
-  name=QDKP2_GetMain(name)
+  local OriginalName = name
+  name = QDKP2_GetMain(name)
 
   if not QDKP2note[name] then
-    QDKP2_Msg(QDKP2_COLOR_RED..QDKP2_LOC_NoPlayerInChance)
+    QDKP2_Msg(QDKP2_COLOR_RED .. QDKP2_LOC_NoPlayerInChance)
     return
   end
 
@@ -189,30 +199,38 @@ function QDKP2_AddTotals(name, award, spent, hours, reason, noMsgIt, timestamp, 
     spent = RoundNum(spent)
   end
   if hours then
-    hours = RoundNum(hours*10)/10
+    hours = RoundNum(hours * 10) / 10
   end
 
-  local Net=QDKP2_GetNet(name)
-  local Gain=0
-  if award then Gain=Gain + award; end
-  if spent then Gain=Gain - spent; end
-  local newNet=Net+Gain
-  if (newNet>QDKP2_MAXIMUM_NET) then
-    if not award then award=0; end
-    award=award-(newNet-QDKP2_MAXIMUM_NET )
-    local Msg=QDKP2_LOC_MaxNetLimit
-    Msg=string.gsub(Msg,"$NAME",name)
-    Msg=string.gsub(Msg,"$MAXIMUMNET",tostring(QDKP2_MAXIMUM_NET))
-    QDKP2_Msg(QDKP2_COLOR_YELLOW..Msg)
+  local Net = QDKP2_GetNet(name)
+  local Gain = 0
+  if award then
+    Gain = Gain + award;
+  end
+  if spent then
+    Gain = Gain - spent;
+  end
+  local newNet = Net + Gain
+  if (newNet > QDKP2_MAXIMUM_NET) then
+    if not award then
+      award = 0;
+    end
+    award = award - (newNet - QDKP2_MAXIMUM_NET)
+    local Msg = QDKP2_LOC_MaxNetLimit
+    Msg = string.gsub(Msg, "$NAME", name)
+    Msg = string.gsub(Msg, "$MAXIMUMNET", tostring(QDKP2_MAXIMUM_NET))
+    QDKP2_Msg(QDKP2_COLOR_YELLOW .. Msg)
     QDKP2log_Event(name, QDKP2_LOC_MaxNetLimitLog)
   end
-  if (newNet<QDKP2_MINIMUM_NET) then
-    if not spent then spent=0; end
-    spent=spent-(QDKP2_MINIMUM_NET - newNet)
-    local Msg=QDKP2_LOC_MinNetLimit
-    Msg=string.gsub(Msg,"$NAME",name)
-    Msg=string.gsub(Msg,"$MINIMUMNET",tostring(QDKP2_MINIMUM_NET))
-    QDKP2_Msg(QDKP2_COLOR_YELLOW..Msg)
+  if (newNet < QDKP2_MINIMUM_NET) then
+    if not spent then
+      spent = 0;
+    end
+    spent = spent - (QDKP2_MINIMUM_NET - newNet)
+    local Msg = QDKP2_LOC_MinNetLimit
+    Msg = string.gsub(Msg, "$NAME", name)
+    Msg = string.gsub(Msg, "$MINIMUMNET", tostring(QDKP2_MINIMUM_NET))
+    QDKP2_Msg(QDKP2_COLOR_YELLOW .. Msg)
     QDKP2log_Event(name, QDKP2_LOC_MinNetLimitLog)
   end
 
@@ -224,14 +242,16 @@ function QDKP2_AddTotals(name, award, spent, hours, reason, noMsgIt, timestamp, 
 
   local HaveModify
 
-  local limitMax,limitMin = QDKP2_GetMaximumFieldNumber()
+  local limitMax, limitMin = QDKP2_GetMaximumFieldNumber()
 
   if award then
     if award ~= 0 then
       DTotal = award
-      local newTotal=award + oldTotal
-      if newTotal>limitMax then newTotal=limitMax
-      elseif newTotal<limitMin then newTotal=limitMin
+      local newTotal = award + oldTotal
+      if newTotal > limitMax then
+        newTotal = limitMax
+      elseif newTotal < limitMin then
+        newTotal = limitMin
       end
       QDKP2note[name][QDKP2_TOTAL] = newTotal
 
@@ -240,11 +260,13 @@ function QDKP2_AddTotals(name, award, spent, hours, reason, noMsgIt, timestamp, 
     end
   end
   if spent then
-    if spent ~= 0  then
+    if spent ~= 0 then
       DSpent = spent
-      local newSpent=spent + oldSpent
-      if newSpent>limitMax then newSpent=limitMax
-      elseif newSpent<limitMin then newSpent=limitMin
+      local newSpent = spent + oldSpent
+      if newSpent > limitMax then
+        newSpent = limitMax
+      elseif newSpent < limitMin then
+        newSpent = limitMin
       end
       QDKP2note[name][QDKP2_SPENT] = newSpent
       Gained = Gained - DSpent
@@ -254,73 +276,84 @@ function QDKP2_AddTotals(name, award, spent, hours, reason, noMsgIt, timestamp, 
   if hours then
     if hours ~= 0 then
       DHours = hours
-      local newHours=hours + oldHours
-      if newHours < 0 then newHours = 0
-      elseif newHours > 999.9 then newHours = 999.9
+      local newHours = hours + oldHours
+      if newHours < 0 then
+        newHours = 0
+      elseif newHours > 999.9 then
+        newHours = 999.9
       end
       QDKP2note[name][QDKP2_HOURS] = newHours
       HaveModify = true
     end
   end
 
-  if not HaveModify and not reason then return; end
+  if not HaveModify and not reason then
+    return ;
+  end
 
   if not noLogIt then
 
-    if Gained == 0 then Gained = nil; end
+    if Gained == 0 then
+      Gained = nil;
+    end
 
-    local sessionOut=QDKP2log_Entry(name,reason, QDKP2LOG_MODIFY, {DTotal ,DSpent, DHours}, timestamp, nil, outsideSession)
+    local sessionOut = QDKP2log_Entry(name, reason, QDKP2LOG_MODIFY, { DTotal, DSpent, DHours }, timestamp, nil, outsideSession)
 
     if not noMsgIt then
-      local description = QDKP2log_GetLastLogText(name,sessionOut)
-      QDKP2_Msg(QDKP2_GetName(OriginalName) .. " " .. description,"DKP")
+      local description = QDKP2log_GetLastLogText(name, sessionOut)
+      QDKP2_Msg(QDKP2_GetName(OriginalName) .. " " .. description, "DKP")
     end
   end
 
-  local Net=QDKP2_GetNet(name)
+  local Net = QDKP2_GetNet(name)
 
-  if QDKP2_AnnounceWhisper and Gained ~= 0 and QDKP2online[OriginalName] then
+  if QDKP2_AnnounceWhisper and Gained ~= 0 then
     local msg
     if reason then
-      msg=QDKP2_LOC_AnnounceWhisperRes
-      msg=string.gsub(msg,'$REASON',reason)
-	  
-	  local lootmethod, masterlooterPartyID, masterlooterRaidID = GetLootMethod()
-	  if lootmethod == "master" and masterlooterPartyID == 0 and GetNumLootItems() ~= 0 then
-	    for ci = 1, 40 do
+      msg = QDKP2_LOC_AnnounceWhisperRes
+      msg = string.gsub(msg, '$REASON', reason)
+
+      local lootmethod, masterlooterPartyID, masterlooterRaidID = GetLootMethod()
+      if lootmethod == "master" and masterlooterPartyID == 0 and GetNumLootItems() ~= 0 and QDKP2online[OriginalName] then
+        for ci = 1, 40 do
           if GetMasterLootCandidate(ci) == OriginalName then
             for li = 1, GetNumLootItems() do
-		      if GetLootSlotLink(li) == reason then
+              if GetLootSlotLink(li) == reason then
                 GiveMasterLoot(li, ci)
-				break
-			  end
+                break
+              end
             end
-			break
+            break
           end
-	    end
-	  end
-	
-    else msg = QDKP2_LOC_AnnounceWhisperTxt
+        end
+      end
+
+    else
+      msg = QDKP2_LOC_AnnounceWhisperTxt
     end
-    if award==0 then award=nil; end
-    if spent==0 then spent=nil; end
+    if award == 0 then
+      award = nil;
+    end
+    if spent == 0 then
+      spent = nil;
+    end
     local awsp = QDKP2_GetAwardSpendText(award, spent)
-    msg=string.gsub(msg,'$AWARDSPENDTXT',awsp)
-    msg=string.gsub(msg,'$NET',tostring(Net))
-    QDKP2_SendHiddenWhisper(msg,OriginalName)
+    msg = string.gsub(msg, '$AWARDSPENDTXT', awsp)
+    msg = string.gsub(msg, '$NET', tostring(Net))
+    QDKP2_SendHiddenWhisper(msg, OriginalName)
   end
 
   if QDKP2_CHANGE_NOTIFY_NEGATIVE then
     if Net < 0 then
-      local msg=string.gsub(QDKP2_LOC_Negative,"$NAME",OriginalName)
-      QDKP2_Msg(msg,'NEGATIVE')
+      local msg = string.gsub(QDKP2_LOC_Negative, "$NAME", OriginalName)
+      QDKP2_Msg(msg, 'NEGATIVE')
     end
   end
 
   if QDKP2_CHANGE_NOTIFY_WENT_NEGATIVE then
     if (Net < 0) and (oldTotal - oldSpent >= 0) then
-      local msg=string.gsub(QDKP2_LOC_GoesNegative,"$NAME",OriginalName)
-      QDKP2_Msg(msg,'GOESNEGATIVE')
+      local msg = string.gsub(QDKP2_LOC_GoesNegative, "$NAME", OriginalName)
+      QDKP2_Msg(msg, 'GOESNEGATIVE')
     end
   end
 
@@ -329,51 +362,74 @@ end
 
 --three dummies of AddTotal for easier reading.
 --they will iterate name if is a list.
-function QDKP2_PlayerGains(name,amount,reason,NoLog,NoMsg)
-  if not QDKP2_OfficerMode() then QDKP2_Msg(QDKP2_LOC_NoRights,"ERROR")(); return; end
-  if type(name)=="string" then name={name}; end
+function QDKP2_PlayerGains(name, amount, reason, NoLog, NoMsg)
+  if not QDKP2_OfficerMode() then
+    QDKP2_Msg(QDKP2_LOC_NoRights, "ERROR")();
+    return ;
+  end
+  if type(name) == "string" then
+    name = { name };
+  end
   QDKP2_DoubleCheckInit()
-  for i,v in pairs(name) do
+  for i, v in pairs(name) do
     if not QDKP2_IsMainAlreadyProcessed(v) then
       QDKP2_AddTotals(v, amount, nil, nil, reason, NoMsg, nil, NoLog)
       QDKP2_ProcessedMain(v)
     end
   end
-  if QDKP2_SENDTRIG_MODIFY then QDKP2_UploadAll(); end
+  if QDKP2_SENDTRIG_MODIFY then
+    QDKP2_UploadAll();
+  end
 end
 
-function QDKP2_PlayerSpends(name,amount,reason,NoLog,NoMsg)
-  if not QDKP2_OfficerMode() then QDKP2_Msg(QDKP2_LOC_NoRights,"ERROR")(); return; end
-  if type(name)=="string" then name={name}; end
+function QDKP2_PlayerSpends(name, amount, reason, NoLog, NoMsg)
+  if not QDKP2_OfficerMode() then
+    QDKP2_Msg(QDKP2_LOC_NoRights, "ERROR")();
+    return ;
+  end
+  if type(name) == "string" then
+    name = { name };
+  end
   QDKP2_DoubleCheckInit()
-  for i,v in pairs(name) do
+  for i, v in pairs(name) do
     if not QDKP2_IsMainAlreadyProcessed(v) then
       QDKP2_AddTotals(v, nil, amount, nil, reason, NoMsg, nil, NoLog)
       QDKP2_ProcessedMain(v)
     end
   end
-  if QDKP2_SENDTRIG_MODIFY then QDKP2_UploadAll(); end
+  if QDKP2_SENDTRIG_MODIFY then
+    QDKP2_UploadAll();
+  end
 end
 
-function QDKP2_PlayerIncTime(name,hours,reason,NoLog,NoMsg)
-  if not QDKP2_OfficerMode() then QDKP2_Msg(QDKP2_LOC_NoRights,"ERROR")(); return; end
-  if type(name)=="string" then name={name}; end
+function QDKP2_PlayerIncTime(name, hours, reason, NoLog, NoMsg)
+  if not QDKP2_OfficerMode() then
+    QDKP2_Msg(QDKP2_LOC_NoRights, "ERROR")();
+    return ;
+  end
+  if type(name) == "string" then
+    name = { name };
+  end
   QDKP2_DoubleCheckInit()
-  for i,v in pairs(name) do
+  for i, v in pairs(name) do
     if not QDKP2_IsMainAlreadyProcessed(v) then
       QDKP2_AddTotals(v, nil, nil, hours, reason, NoMsg, nil, NoLog)
       QDKP2_ProcessedMain(v)
     end
   end
-  if QDKP2_SENDTRIG_MODIFY then QDKP2_UploadAll(); end
+  if QDKP2_SENDTRIG_MODIFY then
+    QDKP2_UploadAll();
+  end
 end
 
-function QDKP2_GetAmountFromText(text,name)
-  text=string.gsub(text,"[ \t]","")
-  if string.sub(text, -1)=="%" then
-    local perc = tonumber(string.gsub(text,'%%','') or '')
-    if perc then text=QDKP2_GetNet(name)*perc/100; end
-    QDKP2_Debug(2,"Core","Award passed as percentual. Calculation is award="..tostring(text))
+function QDKP2_GetAmountFromText(text, name)
+  text = string.gsub(text, "[ \t]", "")
+  if string.sub(text, -1) == "%" then
+    local perc = tonumber(string.gsub(text, '%%', '') or '')
+    if perc then
+      text = QDKP2_GetNet(name) * perc / 100;
+    end
+    QDKP2_Debug(2, "Core", "Award passed as percentual. Calculation is award=" .. tostring(text))
   end
   return tonumber(text)
 end
